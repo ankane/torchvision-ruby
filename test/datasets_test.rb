4cdfd112b0e00ef049c11d656afad530a9d1f27c
Andrew Kane <andrew@chartkick.com>
2020-04-28 00:11:30 -0700

    Fixed tests

--- test/datasets_test.rb
+++ test/datasets_test.rb
@@ -4,12 +4,12 @@ class DatasetsTest < Minitest::Test
   def test_mnist
     trainset = TorchVision::Datasets::MNIST.new(root, train: true, download: true)
     assert_equal 60000, trainset.size
-    assert_kind_of MiniMagick::Image, trainset[0][0]
+    assert_kind_of Torch::Tensor, trainset[0][0]
     assert_equal 5, trainset[0][1]
 
     testset = TorchVision::Datasets::MNIST.new(root, train: false, download: true)
     assert_equal 10000, testset.size
-    assert_kind_of MiniMagick::Image, testset[0][0]
+    assert_kind_of Torch::Tensor, testset[0][0]
     assert_equal 7, testset[0][1]
   end
 
