52fb411ba5d7db03017c94140b987728455d5a38
Andrew Kane <andrew@chartkick.com>
2020-04-29 03:59:19 -0700

    Better ResNet test [skip ci]

--- test/models_test.rb
+++ test/models_test.rb
@@ -2,6 +2,17 @@ require_relative "test_helper"
 
 class ModelsTest < Minitest::Test
   def test_resnet
+    transform = TorchVision::Transforms::Compose.new([
+      TorchVision::Transforms::ToTensor.new,
+      TorchVision::Transforms::Normalize.new([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+    trainset = TorchVision::Datasets::CIFAR10.new(root, train: true, download: true, transform: transform)
+    trainloader = Torch::Utils::Data::DataLoader.new(trainset, batch_size: 4)
+
     net = TorchVision::Models::ResNet18.new
+    trainloader.each do |data, target|
+      net.call(data)
+      break
+    end
   end
 end
