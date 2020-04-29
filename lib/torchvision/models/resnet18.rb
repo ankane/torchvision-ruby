884a1dc59c8c9ded0d36a7003d7015b171cfa927
Andrew Kane <andrew@chartkick.com>
2020-04-29 06:05:35 -0700

    Started support for pretrained models

--- lib/torchvision/models/resnet18.rb
+++ lib/torchvision/models/resnet18.rb
@@ -1,8 +1,14 @@
 module TorchVision
   module Models
     module ResNet18
-      def self.new(**kwargs)
-        ResNet.new(BasicBlock, [2, 2, 2, 2], **kwargs)
+      def self.new(pretrained: false, **kwargs)
+        model = ResNet.new(BasicBlock, [2, 2, 2, 2], **kwargs)
+        if pretrained
+          url = "https://download.pytorch.org/models/resnet18-5c106cde.pth"
+          state_dict = Torch::Hub.load_state_dict_from_url(url)
+          model.load_state_dict(state_dict)
+        end
+        model
       end
     end
   end
