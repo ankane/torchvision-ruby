d112b2cb76b6035348aabb2376353b571803ee52
Andrew Kane <andrew@chartkick.com>
2020-09-19 17:18:21 -0700

    Only call contiguous when needed [skip ci]

--- lib/torchvision/utils.rb
+++ lib/torchvision/utils.rb
@@ -89,7 +89,7 @@ module TorchVision
       def image_from_array(array)
         raise "Expected tensor with dtype :uint8" unless array.is_a?(Torch::Tensor) && array.dtype == :uint8
 
-        array = array.contiguous
+        array = array.contiguous unless array.contiguous?
 
         width, height = array.shape
         bands = array.shape[2] || 1
