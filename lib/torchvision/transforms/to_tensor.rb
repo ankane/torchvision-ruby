9ca1c649922ab15296be98533fdfeba3cc79f853
Andrew Kane <andrew@ankane.org>
2021-03-11 12:08:22 -0800

    Use Torch::NN::Module

--- lib/torchvision/transforms/to_tensor.rb
+++ lib/torchvision/transforms/to_tensor.rb
@@ -1,7 +1,7 @@
 module TorchVision
   module Transforms
-    class ToTensor
-      def call(pic)
+    class ToTensor < Torch::NN::Module
+      def forward(pic)
         F.to_tensor(pic)
       end
     end
