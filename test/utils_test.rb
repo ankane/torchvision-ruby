16103b3e985e1694dfa3c1bd6fa42db98c4e996c
Andrew Kane <andrew@ankane.org>
2025-09-29 18:02:51 -0700

    Fixed test warnings [skip ci]

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -4,7 +4,7 @@ class UtilsTest < Minitest::Test
   def test_save_image
     trainset = TorchVision::Datasets::MNIST.new(root, train: true, download: true, transform: TorchVision::Transforms::ToTensor.new)
     trainloader = Torch::Utils::Data::DataLoader.new(trainset, batch_size: 64)
-    images, labels = trainloader.first
+    images, _labels = trainloader.first
     TorchVision::Utils.save_image(images[0...25], "#{Dir.tmpdir}/image.png", nrow: 5, normalize: true)
   end
 end
