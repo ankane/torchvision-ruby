ee63419d275e082f826496f7f9d9e92b6447fb4a
Andrew Kane <andrew@ankane.org>
2026-04-08 22:19:21 -0700

    Improved test cleanup [skip ci]

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -5,6 +5,8 @@ class UtilsTest < Minitest::Test
     trainset = TorchVision::Datasets::MNIST.new(root, train: true, download: true, transform: TorchVision::Transforms::ToTensor.new)
     trainloader = Torch::Utils::Data::DataLoader.new(trainset, batch_size: 64)
     images, _labels = trainloader.first
-    TorchVision::Utils.save_image(images[0...25], "#{Dir.tmpdir}/image.png", nrow: 5, normalize: true)
+    Dir.mktmpdir do |dir|
+      TorchVision::Utils.save_image(images[0...25], "#{dir}/image.png", nrow: 5, normalize: true)
+    end
   end
 end
